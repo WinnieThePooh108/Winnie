afdd0f8584e7f71b10612f4420fa3822192e14ac
WinnieThePooh108 <72556181+WinnieThePooh108@users.noreply.github.com>
2020-10-08 08:25:30 -0400

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        partials: ['MESSAGE', 'REACTION']
+    }),
+    config = require('./config.json'),
+    fs = require('fs'),
+    humanizeDuration = require('humanize-duration'),
+    cooldown = new Set()
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+client.db = require('./db.json')
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    if (!message.member.hasPermission('MANAGE_CHANNELS') && client.db.lockedChannels.includes(message.channel.id)) return message.delete()
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    if (command.guildOnly && !message.guild) return message.channel.send('Cette commande ne peut être utilisée que dans un serveur.')
+    command.run(message, args, client)
+})
+ 
+client.on('channelCreate', channel => {
+    if (!channel.guild) return
+    const muteRole = channel.guild.roles.cache.find(role => role.name === 'Muted')
+    if (!muteRole) return
+    channel.createOverwrite(muteRole, {
+        SEND_MESSAGES: false,
+        CONNECT: false,
+        ADD_REACTIONS: false
+    })
+})
